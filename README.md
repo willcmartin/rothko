3ef0d6c3ddc8d77001f8f8d5cae3c32d1cc4d43d
Will Martin <wcmartin01@gmail.com>
2021-07-04 19:42:07 -0500

    test source file, very basic lexer

--- README.md
+++ README.md
@@ -1,1 +1,41 @@
-# rothko
+# Rothko
+
+A simple language
+
+---
+
+## General info:
+
+### rothko language
+- .rk extension
+- language rules
+    - all variables are integers
+    - variables can be added
+
+### lexer
+- written in python (eventually c)
+
+---
+
+## Notes:
+
+rothko code file -> lexer -> parser -> compiler/interpreter
+
+### rothko language
+- free-form (white space doesn't matter)
+- compiled or interpreted?
+
+### lexer
+- input: string (source code)
+- output: list of tokens
+- token
+    - things like numbers, strings, operators, punctuation, etc.
+
+---
+
+## Useful sources
+
+- https://accu.org/journals/overload/26/145/balaam_2510/
+- https://www.freecodecamp.org/news/the-programming-language-pipeline-91d3f449c919/
+- https://gist.github.com/eliben/5797351
+- https://github.com/Rakhyvel/TinyLang
---
+++ lexer.py
@@ -0,0 +1,41 @@
+# lexer for Rothko
+# python3 lexer.py test.rk
+
+# very very simple right now
+
+import sys
+import re
+
+class Token():
+    def __init__(self, type, val):
+        self.type = type
+        self.value = val
+    def write(self):
+        print("type: " + self.type + " value: " + self.value)
+
+def lexer(c):
+    if c in " \n":
+        return None
+    elif c == '=':
+        return Token("OP", '=')
+    elif re.match("[_a-zA-Z]", c):
+        return Token("ID", c)
+    elif re.match("[.0-9]", c):
+        return Token("INT", c)
+    else:
+        raise Exception(c + " is not allowed. Be less dumb.")
+
+
+if __name__ == '__main__':
+    tokens = []
+
+    with open(sys.argv[1]) as rk_file:
+        input = rk_file.read()
+
+    for char in input:
+        t = lexer(char)
+        if t is not None:
+            tokens.append(lexer(char))
+
+    for t in tokens:
+        t.write()
