185f8eebbd89132cb5c99a50db19d0eb6056b5a1
Will Martin <wcmartin01@gmail.com>
2021-07-14 23:26:16 -0500

    the parser works for while loops, but at what cost?

--- parser.py
+++ parser.py
@@ -6,7 +6,7 @@ from stream import stream
 
 # TODO: should be function in parse function then don't have to pass tokens
 # TODO: remove get_next function?
-def build_ast(tokens, prev_ast, get_next=True):
+def build_ast(prev_ast, get_next=True):
     if get_next == True:
         tokens.get_next()
 
@@ -16,17 +16,32 @@ def build_ast(tokens, prev_ast, get_next=True):
     # recursive step
     else:
         if tokens.curr.type in ("IDENTIFIER", "INTEGER"):
-            return build_ast(tokens, [tokens.curr.type, tokens.curr.val])
+            return build_ast([tokens.curr.type, tokens.curr.val])
+        elif tokens.curr.type in ("KEYWORD"):
+            if tokens.curr.val in ("while"):
+                return build_ast(["while", build_ast(None), build_ast(None)], False)
+            if tokens.curr.val in ("endwhile"):
+                return build_ast(None)
         elif tokens.curr.type in ("OPERATOR"):
             if tokens.curr.val in ("="):
-                next_expression = build_ast(tokens, None)
-                return build_ast(tokens, ["assignment", "=", prev_ast, next_expression], False)
-            if tokens.curr.val in ("+"):
-                next_expression = build_ast(tokens, None)
-                return build_ast(tokens, ["operation", "+", prev_ast, next_expression], False)
+                next_expression = build_ast(None)
+                return build_ast(["assignment", "=", prev_ast, next_expression], False)
+            elif tokens.curr.val in ("+"):
+                next_expression = build_ast(None)
+                return build_ast(["operation", "+", prev_ast, next_expression], False)
+            elif tokens.curr.val in ("=="):
+                next_expression = build_ast(None)
+                return build_ast(["bool", "==", prev_ast, next_expression], False)
+        elif tokens.curr.val in ("()"):
+            if tokens.curr.val == "(":
+                return build_ast(None)
+            elif tokens.curr.val == ")":
+                return prev_ast
+
 
 def parse(token_stream):
+    global tokens
     tokens = stream(token_stream)
     while tokens.curr is not None:
-        yield build_ast(tokens, None, False)
+        yield build_ast(None, False)
         tokens.get_next()
--- rothko.py
+++ rothko.py
@@ -11,6 +11,6 @@ if __name__ == "__main__":
     rk_file = sys.argv[1]
     env = Enviornment()
     # evaluate(parse(lex(read_file(rk_file))), env)
-    for a in lex(read_file(rk_file)):
+    for a in parse(lex(read_file(rk_file))):
         print(a)
     # run(sys.argv[1])
